440c8c80b1a4070501696011ee0b2c1a856ffb90
Michal C <michal1024@gmail.com>
2021-09-12 10:25:55 +0200

    refactoring current weather

--- lib/weather/current_weather.dart
+++ lib/weather/current_weather.dart
@@ -7,73 +7,74 @@ import 'weather_provider.dart';
 import 'model/location_forecast.dart';
 
 class CurrentWeather extends StatelessWidget {
-  final _mainStyle = TextStyle(fontSize: 72);
   final _detailStyle = TextStyle(fontSize: 24);
-  @override
-  Widget build(BuildContext ctx) {
-    return Consumer<Weather>(
-        builder: (context, weather, child) => buildWidget(weather));
-  }
 
-  Widget buildWidget(Weather weather) {
-    var data = weather.forecast?.properties.timeseries[0].data;
-    var now = data?.instant;
-    var next = data?.next_1_hours ?? data?.next_6_hours ?? data?.next_12_hours;
+  @override
+  Widget build(BuildContext ctx) => Consumer<Weather>(builder: _builder);
+
+  Widget _builder(BuildContext ctx, Weather weather, Widget? child) {
+    /*TODO:
+    change .now and .next to .at(Datetime)
+    return new DataEntry that combines parameters from now.instant and summary from next.summary
+    */
+    var now = weather.now;
     return now == null
         ? Text('No data')
         : Column(
-            children: [
-              currentTempAndIcon(now, next), 
-              conditions(now, next)],
+            children: [CurrentCondition(now: now, next:weather.next), 
+            conditions(now, weather.next)],
             mainAxisAlignment: MainAxisAlignment.center,
           );
   }
 
-  Widget currentTempAndIcon(DataEntry now, DataEntry? next) {
+  Widget conditions(DataEntry now, DataEntry? next) => Row(
+        children: <Widget>[
+          windSpeedDir(now),
+          humidity(now),
+          pressure(now),
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      );
+
+  Widget windSpeedDir(DataEntry now) => Row(children: [
+        Arrow(direction: now.details?.wind_from_direction ?? 0),
+        Text('${now.details?.wind_speed} m/s', style: _detailStyle),
+      ]);
+
+  Widget humidity(DataEntry now) =>
+      Text('${now.details?.relative_humidity} %', style: _detailStyle);
+
+  Widget pressure(DataEntry now) =>
+      Text('${now.details?.air_pressure_at_sea_level} hPa',
+          style: _detailStyle);
+}
+
+class CurrentCondition extends StatelessWidget {
+  final DataEntry now;
+  final DataEntry? next;
+  CurrentCondition({required this.now, this.next});
+
+  @override
+  Widget build(BuildContext ctx) {
+    String temp = now.details?.air_temperature?.toString() ?? '';
+    String symbol = next?.summary?.symbol_code ?? 'clearsky_day';
     return Row(
-      children: [
-        temperature(now), 
-        if (next != null) icon(next)],
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text('$temp℃', style: TextStyle(fontSize: 72)), 
+          Image(image: AssetImage('images/weathericon/$symbol.png'))],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     );
   }
+}
 
-  Widget temperature(DataEntry now) {
-    return Text('${now.details?.air_temperature}℃',
-        style: _mainStyle);
-  }
+class Arrow extends StatelessWidget {
+  final double direction;
+  Arrow({required this.direction});
 
-  Widget icon(DataEntry next) {
-    var symbol = next.summary?.symbol_code ?? 'clearsky_day';
-    return Image(image: AssetImage('images/weathericon/$symbol.png'));
-  }
-
-//arrow_downward_rounded
-  Widget conditions(DataEntry now, DataEntry? next) {
-    return Row(
-      children: <Widget>[
-        windSpeedDir(now),
-        humidity(now),
-        pressure(now),
-      ],
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    );
-  }
-
-  Widget windSpeedDir(DataEntry now) {
-    double direction = now.details?.wind_from_direction ?? 0;
+  @override
+  Widget build(BuildContext ctx) {
     double rads = direction * pi / 180.0;
-    return Row(children: [
-      Transform.rotate(angle: rads, child: Icon(Icons.arrow_downward_rounded)),
-      Text('${now.details?.wind_speed} m/s', style: _detailStyle),
-    ]);
-  }
-
-  Widget humidity(DataEntry now) {
-    return Text('${now.details?.relative_humidity} %', style: _detailStyle);
-  }
-
-  Widget pressure(DataEntry now) {
-    return Text('${now.details?.air_pressure_at_sea_level} hPa', style: _detailStyle);
+    return Transform.rotate(
+        angle: rads, child: Icon(Icons.arrow_downward_rounded));
   }
 }
--- lib/weather/weather_provider.dart
+++ lib/weather/weather_provider.dart
@@ -7,7 +7,14 @@ class Weather extends ChangeNotifier {
   LocationForecast? _forecast;
   LocationForecast? get forecast => _forecast;
 
-  void updateTime(dynamic data) {
+  DataEntry? get now => forecast?.properties.timeseries[0].data.instant;
+
+  DataEntry? get next {
+    var data = forecast?.properties.timeseries[0].data;
+    return data?.next_1_hours ?? data?.next_6_hours ?? data?.next_12_hours;
+  }
+
+  void updateForecast(dynamic data) {
     _forecast = LocationForecast.fromJson(data);
     notifyListeners();
   }
@@ -20,6 +27,6 @@ class Weather extends ChangeNotifier {
                 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=49.97&lon=20.15'),
             headers: {'User-Agent': 'hobby project/0 michal1024@gmail.com'})
         .then((response) => jsonDecode(response.body))
-        .then(updateTime);
+        .then(updateForecast);
   }
 }
