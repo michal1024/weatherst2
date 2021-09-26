f68b4eee94f025655a2e677d7511f8610374ecf8
Michal C <michal1024@gmail.com>
2021-09-26 11:31:38 +0200

    Error handling

--- lib/weather/weather_provider.dart
+++ lib/weather/weather_provider.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'package:flutter/widgets.dart';
+import 'package:logger/logger.dart';
 import 'dart:convert';
 import 'dart:async';
 import 'dart:io';
@@ -8,40 +9,64 @@ import 'model/location_forecast.dart';
 class Weather extends ChangeNotifier {
   LocationForecast? _forecast;
   LocationForecast? get forecast => _forecast;
-  //Timer? _timer;
+  Timer? _timer;
   DateTime _expires = DateTime.fromMillisecondsSinceEpoch(0);
-  DateTime _lastRun = DateTime.fromMillisecondsSinceEpoch(0);
+  DateTime _lastFetch = DateTime.fromMillisecondsSinceEpoch(0);
+  Logger _logger = Logger();
   
   Weather() {
     Timer.run(_fetchForecast);
-    Timer.periodic(Duration(hours: 1), (_) => _fetchForecast());
+    _timer = Timer.periodic(Duration(hours: 1), (_) => _fetchForecast());
+  }
+  
+  @override
+  dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
     
   void _fetchForecast() {
-    if (_lastRun.isBefore(_expires)) {
+    if (_lastFetch.isBefore(_expires)) {
+      _logger.i('''Skip forecast fetching, 
+          expires: ${_expires.toIso8601String()}
+          last fetch: ${_lastFetch.toIso8601String()}
+        ''');
       return;
     }
-    http
-        .get(
-            Uri.parse(
-                'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=49.97&lon=20.15'),
-            headers: {'User-Agent': 'hobby project/0 michal1024@gmail.com',
-              'If-Modified-Since': HttpDate.format(_lastRun)})
-        .then(_processResponse);
+    try {
+      http.get(
+          Uri.parse(
+              'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=49.97&lon=20.15'),
+          headers: {
+            'User-Agent': 'hobby project/0 michal1024@gmail.com',
+            'If-Modified-Since': HttpDate.format(_lastFetch)
+          }).then(_processResponse);
+    } catch (e) {
+      _logger.e('Failed to fetch forecast', e);
+    }
   }
 
   void _processResponse(http.Response response) {
-    _updateExpiration(response);
-    _updateForecast(response);
-    _lastRun = DateTime.now();
-    notifyListeners();
+    _logger.i('Fetch status code ${response.statusCode}');
+    if (response.statusCode != 200) {
+      return;
+    }
+    try {
+      _updateExpiration(response);
+      _updateForecast(response);
+      _lastFetch = DateTime.now();
+      notifyListeners();
+    } catch (_) {
+
+    }
   }
 
   void _updateExpiration(http.Response response) {
     if (response.headers.containsKey('Expires')) try {
       _expires = HttpDate.parse(response.headers['Expires']!);
-    } catch(_) {
-
+    } catch(e) {
+      _logger.e('Parsing Expires header failed', e);
+      rethrow;
     }
   }
 
@@ -49,8 +74,10 @@ class Weather extends ChangeNotifier {
     try {
       var data = jsonDecode(response.body);
       _forecast = LocationForecast.fromJson(data);
-    } catch (_) {
-      
+
+    } catch (e) {
+      _logger.e('Failed to parse forecast response', e);
+      rethrow;
     }
   }
 }
